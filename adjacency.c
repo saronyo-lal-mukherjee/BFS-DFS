99f5e75cd6503d2344106c34aff510ecaec1eb3f
Nitin De <66069955+Nimo-GitHub@users.noreply.github.com>
2020-06-07 10:07:14 -0700

    Adjacency Matrix using stucture definition

--- adjacency.c
+++ adjacency.c
@@ -1,66 +1,78 @@
+//Library Definition
+
 #include<stdio.h>
 #include<stddef.h>
 
+//Constant Definition
 #define maxv 10
 
-//Function Definitions
+//Structure Definition
+typedef struct adjacency_matrix
+{
+    int v;
+    int mat[maxv][maxv];
+}adj_mat;
 
-int** adj_matrix();
-void display_adj_matrix(int**);
+
+//Function Definition
+
+adj_mat* adj_matrix();
+void display_adj_matrix(adj_mat* a);
+
+//Main Function
 
 int main()
 {
     display_adj_matrix(adj_matrix());
-	printf("Yayy");	
 }
 
 
 
 
-
-
-int** adj_matrix()
+adj_mat* adj_matrix()
 {
-	int i,j,v,e,x,y;
-	int a[maxv][maxv];
-	printf("Enter Number of Vertices in the graph : ");
-	scanf("%d",&v);
-	for( i = 0 ; i < v ; i++){
-        	for( j = 0 ; j < v ; j++){
-			a[i][j]=0;
-		}
-	}
+    int i,j,v,e,x,y;
+    adj_mat* a;
+    a = (adj_mat*)malloc(sizeof(adj_mat));
+
+    printf("Enter Number of Vertices in the graph : ");
+    scanf("%d",&v);
+    a->v = v;
+    for( i = 0 ; i < v ; i++)
+        for( j = 0 ; j < v ; j++) a->mat[i][j]=0;
     
-	printf("Enter number of edges :");
-	scanf("%d",&e);
-	printf("%d",a[0][0]);
-	for( i = 0 ; i < e ; i++){
-		printf("Enter node pairs to initialize edges : ");
-        	scanf("%d %d",&x,&y);
-        	a[x][y] = 1;
-        	a[y][x] = 1;
-    	}
-    	return a;
+    printf("Enter number of edges :");
+    scanf("%d",&e);
+
+    for( i = 0 ; i < e ; i++)
+    {
+        printf("Enter node pairs to initialize edges : ");
+        scanf("%d %d",&x,&y);
+        a->mat[x-1][y-1] = 1;
+        a->mat[y-1][x-1] = 1;
+    }
+    
+    return a;
 }
 
-void display_adj_matrix(int** a)
+void display_adj_matrix(adj_mat* a)
 {
-	int size,i,j;
-    	size=5;
-	printf("Adjacency Matrix\n---------------\n");
-	
-	for(i=0;i<size;i++)
-	{
-		
-		printf("|");
-		
-        	for(j=0;j<size;j++)
-        	{
-			
-            		printf("  %d  " , a[i][j]);
-			printf("\nDONE\n");
-        	}
-        	printf("|\n");
-	} 
-	printf("----------------");
+    int size = a->v , i,j;
+    printf("Adjacency Matrix\n");
+    if(a == NULL) 
+    {
+        printf("No Matrix Avaliable");
+        return;
+    }
+        
+    for(i=0;i<size;i++)
+    {
+        printf("|");
+        for(j=0;j<size;j++)
+        {
+            printf("  %d  " , a->mat[i][j]);
+        }
+        printf("|\n");
+    } 
+    
 }
